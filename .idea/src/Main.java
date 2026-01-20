1abc08f849650d0c1a91ad59218abfe8d5d57687
dta5189 <dta5189@psu.edu>
2026-01-20 11:44:41 -0500

    Added my objects

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -3,8 +3,12 @@ public class Main
 {    //this is the main entry point
     public static void main (String[] args){
 
-        Motorcycle m1 = new Motorcycle();
+        Motorcycle m1 = new Motorcycle("RED");//object, pass in the constructor details(color) automatically adds in variable name
+        System.out.println(m1.getColor());
 
+
+        Motorcycle m2 = new Motorcycle("BLUE");
+        System.out.println(m2.getColor());
     }
 
 }
--- .idea/src/Motorcycle.java
+++ .idea/src/Motorcycle.java
@@ -2,6 +2,13 @@ public class Motorcycle
 {
     private String color;
 
+    public Motorcycle(String color){
+        this.color = color;
 
+    }
+
+    public String getColor(){
+        return this.color;
+    }
 
 }
